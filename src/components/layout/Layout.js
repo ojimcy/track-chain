fe0cf904f47b89cee0dffe256390786520169119
Emmanuel <ojimcy247@gmail.com>
2024-08-10 03:50:19 +0100

    update nav

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,79 +2,56 @@ import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Footer from './footer/Footer';
 import { WebappContext } from '../../context/telegram';
-import { getUserByTelegramID } from '../../lib/server';
-import { useTelegramUser } from '../../hooks/telegram';
-import { Spinner } from 'reactstrap';
-import { toast } from 'react-toastify';
-import MainNav from './header/MainNav';
+import { createAccount, getUserByTelegramID } from '../../lib/server';
+import {
+  useTelegramUser,
+  // useCurrentUser,
+  useInitData,
+} from '../../hooks/telegram';
+import MiningNav from './header/MainNav';
 
 const Layout = ({ children }) => {
-  //const initData = useInitData();
-  const {
-    webapp,
-    setUser,
-    loadingPageIsVissible,
-    hideLoadingPage,
-    showLoadingPage,
-  } = useContext(WebappContext);
+  const initData = useInitData();
+  const { setUser } = useContext(WebappContext);
+  // const currentUser = useCurrentUser();
   const telegramUser = useTelegramUser();
+  //const toast = useToast();
 
   useEffect(() => {
-    if (!telegramUser) {
-      // hideLoadingPage();
-      return;
-    }
+    if (!telegramUser) return;
     // if (currentUser) return;
-    showLoadingPage();
-
     const fn = async () => {
-      console.log('telegramUser', telegramUser);
-      if (webapp.expand) webapp.expand();
       let user = await getUserByTelegramID(telegramUser.id);
-      console.log('logged in user', user);
       if (!user || !user.id) {
-        toast.error('Refresh and try again', {
-          position: 'top-right',
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-        });
-
-        return;
+        user = await handleCreateAccount();
       }
       setUser(user);
-      hideLoadingPage();
     };
 
     fn();
   }, [telegramUser]);
 
+  const handleCreateAccount = async () => {
+    const userData = {
+      telegramId: telegramUser.id,
+      username: telegramUser.username,
+      pin: '0000',
+      uplineId: parseInt(initData.start_param) || 0,
+    };
+
+    const user = await createAccount(userData);
+    if (user && user.id) {
+      setUser(user);
+    } else {
+      //toast.error('Error in creating');
+    }
+  };
+
   return (
     <div className="page-content">
-      {!loadingPageIsVissible && (
-        <>
-          <MainNav />
-          <main className="content">{children}</main>
-          <Footer />
-        </>
-      )}
-
-      {loadingPageIsVissible && (
-        <div className="page-content">
-          <main className="content">
-            <div
-              className="d-flex justify-content-center align-items-center"
-              style={{ height: '100vh' }}
-            >
-              <Spinner
-                style={{ width: '3rem', height: '3rem' }}
-                color="primary"
-              />
-              {/* You can change 'primary' to any other color theme like secondary, success, info, etc. */}
-            </div>
-          </main>
-        </div>
-      )}
+      <MiningNav />
+      <main className="main-layout-content">{children}</main>
+      <Footer />
     </div>
   );
 };
