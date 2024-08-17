2c4468a5c287979f388ba143733aee2c5a73c3ce
Emmanuel <ojimcy247@gmail.com>
2024-08-17 13:28:28 +0100

    tst

--- src/components/modals/CardDetailsModal.js
+++ src/components/modals/CardDetailsModal.js
@@ -1,5 +1,4 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
-import Confetti from 'react-confetti';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 import './modal.css';
@@ -7,51 +6,30 @@ import { Separator } from '../common/Seperator';
 
 import dollar from '../../assets/images/dollar.png';
 import { formatBalance } from '../../utils/formatBalance';
-import { getUserByTelegramID, upgradeCard } from '../../lib/server';
+import { upgradeCard } from '../../lib/server';
 import { toast } from 'react-toastify';
-import { useCurrentUser, useTelegramUser } from '../../hooks/telegram';
-import { WebappContext } from '../../context/telegram';
+import { useCurrentUser } from '../../hooks/telegram';
 
 const CardDetailsModal = ({ isOpen, toggle, card }) => {
   const currentUser = useCurrentUser();
-  const { setUser } = useContext(WebappContext);
-  const telegramUser = useTelegramUser();
 
-  const [showConfetti, setShowConfetti] = useState(false);
-  console.log('card', card);
+  if (!card) return null;
 
-  const upgradeCost = card?.upgradeCost || card?.initialUpgradeCost;
-  const hmr = card?.hmr || card?.initialHMR;
-  const insufficientBalance = currentUser.balance < upgradeCost;
-
-  const fetchUserData = useCallback(async () => {
-    try {
-      const user = await getUserByTelegramID(telegramUser.id);
-      setUser(user);
-    } catch (error) {
-      console.error('Failed to fetch user data:', error);
-    }
-  }, [telegramUser, setUser]);
-
-  useEffect(() => {
-    if (telegramUser) {
-      fetchUserData();
-    }
-  }, [telegramUser, fetchUserData]);
+  const insufficientBalance =
+    currentUser.balance < (card.upgradeCost || card.initialUpgradeCost);
+  const hmr = card.hmr || card.initialHMR;
+  const upgradeCost = card.upgradeCost || card.initialUpgradeCost;
 
   const handleCardUpgrade = async () => {
     try {
-      const res = await upgradeCard(card?.id);
+      const res = await upgradeCard(card.id);
       console.log('upgraded card', res);
-      toast.success(`${card?.name} upgraded to level ${card?.level + 1}`, {
+      toast.success(`${card.name} upgraded to level ${card.level + 1}`, {
         position: 'top-right',
         autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
       });
-      setShowConfetti(true);
-      setTimeout(() => setShowConfetti(false), 3000);
-      await fetchUserData();
       toggle();
     } catch (error) {
       console.error('Error while upgrading card', error);
@@ -66,28 +44,13 @@ const CardDetailsModal = ({ isOpen, toggle, card }) => {
 
   return (
     <Modal isOpen={isOpen} toggle={toggle} className="main-modal">
-      {showConfetti && (
-        <Confetti
-          drawShape={(ctx) => {
-            ctx.beginPath();
-            for (let i = 0; i < 22; i++) {
-              const angle = 0.35 * i;
-              const x = (0.2 + 1.5 * angle) * Math.cos(angle);
-              const y = (0.2 + 1.5 * angle) * Math.sin(angle);
-              ctx.lineTo(x, y);
-            }
-            ctx.stroke();
-            ctx.closePath();
-          }}
-        />
-      )}
       <ModalHeader toggle={toggle} className="card-modal-header"></ModalHeader>
       <ModalBody className="text-center">
         <div className="card-image-wrapper mb-3">
-          <img src={card?.image} alt={card?.name} className="card-image" />
+          <img src={card.image} alt={card.name} className="card-image" />
         </div>
-        <h4>{card?.name}</h4>
-        <p className="card-description">{card?.description}</p>
+        <h4>{card.name}</h4>
+        <p className="card-description">{card.description}</p>
         <Separator />
         <div
           className={`card-earnings ${
@@ -106,9 +69,11 @@ const CardDetailsModal = ({ isOpen, toggle, card }) => {
           disabled={insufficientBalance}
         >
           <img src={dollar} alt="" width={35} />{' '}
-          {insufficientBalance
-            ? 'Insufficient Balance'
-            : `${formatBalance(upgradeCost)} Points`}
+          {insufficientBalance ? (
+            'Insufficient Balance'
+          ) : (
+            <>{formatBalance(upgradeCost)} Points</>
+          )}
         </Button>
       </ModalBody>
     </Modal>
