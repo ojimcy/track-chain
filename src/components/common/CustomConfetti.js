2e3d4e7464705faa000f7ba077d4f1493df922b0
Emmanuel <ojimcy247@gmail.com>
2024-09-23 20:00:06 +0100

    undo

--- src/components/common/CustomConfetti.js
+++ src/components/common/CustomConfetti.js
@@ -1,11 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Confetti from 'react-confetti';
 
 import dollar from '../../assets/images/dollar.png';
 
 function ReactConfetti() {
   const imageRef = useRef(null);
-  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
 
   useEffect(() => {
     const img = new Image();
@@ -13,31 +12,10 @@ function ReactConfetti() {
     img.onload = () => {
       imageRef.current = img;
     };
-
-    // Update the window size when resized
-    const handleResize = () => {
-      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
     <Confetti
-      width={windowSize.width}
-      height={windowSize.height}
-      numberOfPieces={300} // Increased pieces for more visual impact
-      gravity={0.4} // Adjust gravity to control fall speed
-      wind={0.05} // Slight wind to add some horizontal movement
-      confettiSource={{
-        x: 0,
-        y: 0,
-        w: windowSize.width,
-        h: 0, // Confetti starts falling from the top
-      }}
-      recycle={false} // Confetti falls once
       drawShape={(ctx) => {
         if (imageRef.current) {
           const size = 15; 
